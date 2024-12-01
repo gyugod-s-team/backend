8b4b0e2b70ded86eb89bea82c9b05caa72c2e702
flowjiyun <gyp2795@gmail.com>
2024-12-01 21:10:04 +0900

    feat: 초기 서버 실행 구현

--- src/be-core/src/main.rs
+++ src/be-core/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world!" }));
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on: {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
